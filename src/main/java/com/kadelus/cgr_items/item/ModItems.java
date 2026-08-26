ab117c150a9ff259173d9a4997e1ed7a0de725ce
BoCoffical <boringcucumber@outlook.com>
2026-08-26 16:20:04 +0800

    Add resonance chisel item(2)

--- src/main/java/com/kadelus/cgr_items/item/ModItems.java
+++ src/main/java/com/kadelus/cgr_items/item/ModItems.java
@@ -2,39 +2,44 @@ package com.kadelus.cgr_items.item;
 
 import com.kadelus.cgr_items.CgrItemsMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.AttributeModifiersComponent;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item WOODEN_HANDLE = new Item(new Item.Settings());
 
-    // 注册木柄物品
-    public static final Item WOODEN_HANDLE = registerItem(
-            "wooden_handle",
-            new Item(new Item.Settings())
-    );
+    public static final Item RESONANCE_CHISEL = new Item(new Item.Settings()
+            .maxCount(1)
+            .component(DataComponentTypes.ATTRIBUTE_MODIFIERS,
+                    AttributeModifiersComponent.builder()
+                            .add(EntityAttributes.GENERIC_ATTACK_DAMAGE,
+                                    new EntityAttributeModifier(
+                                            Identifier.of(CgrItemsMod.MOD_ID, "resonance_chisel_damage"),
+                                            3.0,
+                                            EntityAttributeModifier.Operation.ADD_VALUE),
+                                    net.minecraft.entity.attribute.EntityAttributeModifierSlot.MAINHAND)
+                            .build()));
 
-    // 注册方法
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(
-                Registries.ITEM,
-                Identifier.of(CgrItemsMod.MOD_ID, name),
-                item
-        );
-    }
-
-    // 批量注册
     public static void registerItems() {
-        // 添加到"工具"物品栏分组
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
-            entries.add(WOODEN_HANDLE);
-        });
+        Registry.register(Registries.ITEM,
+                Identifier.of(CgrItemsMod.MOD_ID, "wooden_handle"),
+                WOODEN_HANDLE);
 
-        // 也可以添加到"原料"物品栏
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(WOODEN_HANDLE);
+        Registry.register(Registries.ITEM,
+                Identifier.of(CgrItemsMod.MOD_ID, "resonance_chisel"),
+                RESONANCE_CHISEL);
+
+        // 将共鸣凿加入工具物品组
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
+            entries.add(RESONANCE_CHISEL);
         });
     }
 }
